6f8d2aad2797e5e73cf4a2a9ede7c43c2c362d2d
Yokota Yoshinori <yokota@yynet.org>
2025-07-14 20:20:54 +0900

    feat: Use user.access_at for timestamp in latest_users.py
    Refactor `contrib/latest_users.py` to use `user.access_at` as the timestamp for user activity instead of a fixed `base_ts`. This provides more accurate and dynamic tracking of the latest user access.

--- contrib/latest_users.py
+++ contrib/latest_users.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
-import datetime
 
 if os.path.dirname(__file__).endswith('contrib'):
     sys.path.append(os.path.dirname(__file__)[:-7])
@@ -21,15 +20,11 @@ User = get_user_model()
 
 # VERBOSE = False
 
-# 比較の基準となる、十分に古いawareな日付を用意
-# スクリプト実行時のタイムゾーンを使う
-base_ts = datetime.datetime(2000, 1, 1, tzinfo=timezone.get_current_timezone())
-
 # 現時刻
 now_ts = timezone.now()
 
 for user in User.objects.filter(is_active=True):
-    ts = base_ts
+    ts = user.access_at
 
     # UserLog の最新レコードを取得（なければ None が返る）
     log = UserLog.objects.filter(user=user).order_by('-created_at').first()
